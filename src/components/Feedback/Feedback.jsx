55bc39eed329bfac47e90f13db5a721d5da66295
Ivaskiv <viraivaskiv@gmail.com>
2024-01-27 17:29:49 +0200

    Update Feedback.jsx

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,6 +1,6 @@
 import css from './Feedback.module.css';
 
-export default function Feedback({ good, neutral, bad, total, positive }) {
+export default function Feedback({ good, neutral, bad, total, positivePercentage }) {
   return (
     <>
       <ul className={css.feedbackList}>
@@ -8,7 +8,7 @@ export default function Feedback({ good, neutral, bad, total, positive }) {
         <li className={css.feedbackItem}>Neutral: {neutral}</li>
         <li className={css.feedbackItem}>Bad: {bad} </li>
         <li className={css.feedbackItem}>Total: {total}</li>
-        <li className={css.feedbackItem}>Positive: {positive}</li>
+        <li className={css.feedbackItem}>Positive: {positivePercentage}</li>
       </ul>
     </>
   );
